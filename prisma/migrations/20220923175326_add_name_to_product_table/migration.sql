0a8891072b9c7c00b8dc409d7005f61e05f48624
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-10-04 09:22:33 -0300

    feat: added placeholder login api functions.

--- prisma/migrations/20220923175326_add_name_to_product_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `productName` to the `products` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "products" ADD COLUMN     "productName" TEXT NOT NULL;
